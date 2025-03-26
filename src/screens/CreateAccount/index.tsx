78bf14bbea0f14f2d342756504602b3c1e2e29bc
Lucas Benkendorf <benklucass@gmail.com>
2025-03-25 21:45:56 -0300

    ajustado tela de criar conta

--- src/screens/CreateAccount/index.tsx
+++ src/screens/CreateAccount/index.tsx
@@ -1,24 +1,49 @@
-import { Text, View,TouchableOpacity, StyleSheet, TextInput } from "react-native";
+import React, {useState} from "react";
+import { Text, View,TouchableOpacity, StyleSheet, TextInput,Alert } from "react-native";
 
 
 export default function CreateAccount(){
+    
+    const [name, setName] = useState ('')
+    const [email, setEmail] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    
+    const handleCreateAccount = () => {
+        if(!name) {
+          Alert.alert('Aviso', 'Preencha o campo nome completo')
+        } else if(!email) {
+          Alert.alert('Aviso', 'Preencha o campo email')
+            } else if(!/^\d{3}.\d{3}.\d{3}-\d{2}$/.test(cpf)) {
+          Alert.alert('Aviso', 'Preencha o campo CPF no formato válido. Ex: 000.000.000-00');
+        } else if (password !== confirmPassword){
+            Alert.alert('Aviso', 'as senhas não coincidem!');
+        }else {
+            Alert.alert('Sucesso', 'Conta criada com sucesso!')
+        }
+    }
+
     return(
         <View style={styles.container}> 
             
-            <View style={styles.viewLogin}>
-            <TextInput style={styles.input}  placeholder="CPF" keyboardType="number-pad"/>
-            <TextInput style={styles.input}  placeholder="Password" keyboardType="number-pad" secureTextEntry/>
-            <TouchableOpacity>
-            <Text style={styles.linkText}>Esqueceu sua senha ?</Text>
-            </TouchableOpacity>
+            <View>
+                <Text style={styles.createAccountText}>Criar Conta</Text>
+                <Text style={styles.descriptionText}>Crie uma conta agora e ganhe seu cartão de crédito com R$ 1000,00 de limite</Text>
+            <TextInput style={styles.input}  placeholder="Nome Completo" value={name} onChangeText={setName}/>
+            <TextInput style={styles.input}  placeholder="Email" keyboardType="email-address" value={email} onChangeText={setEmail}/>
+            <TextInput style={styles.input}  placeholder="CPF" keyboardType="number-pad" value={cpf} onChangeText={setCpf}/>
+            <TextInput style={styles.input}  placeholder="Password" secureTextEntry value={password} onChangeText={setPassword}/>
+            <TextInput style={styles.input}  placeholder="Confirm Password"  secureTextEntry value={confirmPassword} onChangeText={setConfirmPassword}/>
+        
             </View>
         
-            <TouchableOpacity style={styles.buttonLogin}>
-                <Text>Entrar</Text>
+            <TouchableOpacity onPress={handleCreateAccount} style={styles.buttonLogin}>
+                <Text>Criar Conta</Text>
             </TouchableOpacity>
         
             <TouchableOpacity >
-            <Text style={[styles.linkText, {textAlign:"center"}]}>Criar conta</Text>
+            <Text style={[styles.linkText, {textAlign:"center"}]}>Já tem uma conta? Faça o login!</Text>
             </TouchableOpacity>
         
         </View>
@@ -47,14 +72,21 @@ const styles = StyleSheet.create({
         fontSize:15,
         marginBottom:40
     },
-    viewLogin:{
-
+    createAccountText:{
+        color:"#FFF",
+        fontSize:30,
+        marginBottom:20,
+    },
+    descriptionText:{
+        color:"#FFF",
+        marginBottom:20,
     },
     buttonLogin:{
         backgroundColor: "#FFF",
         padding: 10,
         borderRadius: 5,
         marginBottom:25,
+        marginTop:25,
         alignItems:"center"
     }
 })
