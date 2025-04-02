8233eea6c048fe8a7338aaf4fd61ba924e2b490f
Lucas Benkendorf <benklucass@gmail.com>
2025-04-02 20:32:22 -0300

    atualizado tela de home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,32 +1,69 @@
+import React, { useEffect, useState } from "react";
 import {
   View,
   StyleSheet,
   Alert,
-  Dimensions,
   ScrollView,
+  Dimensions,
   Text,
+  FlatList,
 } from "react-native";
 import Header from "./Header";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 interface Card {
   numero: string;
   nome: string;
   cvv: string;
-  dataExpiração: string;
+  dataExpiracao: string;
+}
+
+enum TransactionType {
+  ENTRADA = "entrada",
+  SAIDA = "saida",
+}
+
+interface Transaction {
+  data: string;
+  items: {
+    id: number;
+    data: string;
+    tipo: TransactionType;
+    descricao: string;
+    valor: string;
+  }[];
 }
 
 const Home = () => {
-  const [cards, setCards] = useState([Card]);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  //   const getCards = async () => {
+  //     try {
+  //         const response = await axios.get("http://192.168.0.37:3000/cartoes")
+  //         setCards(response.data)
+  //     } catch (error) {
+
+  //     }
+  //   }
 
   useEffect(() => {
     axios
-      .get("http://192.168.3.5:3000/cartoes")
+      .get("http://192.168.0.37:3000/cartoes")
       .then((response) => {
         setCards(response.data);
       })
-      .catch(() => Alert.alert("Erro ao pegar os cartões!"));
+      .catch(() => Alert.alert("Erro ao pegar os cartões"));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.0.37:3000/transacoes")
+      .then((response) => {
+        setTransactions(response.data);
+      })
+      .catch(() => Alert.alert("Erro ao pegar as transações"));
   }, []);
 
   return (
@@ -34,25 +71,57 @@ const Home = () => {
       <Header />
 
       <Text style={styles.myCards}>Meus cartões</Text>
-      <View style={{ height: 210 }}>
-        <ScrollView horizontal>
+      <View style={{ height: 220 }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           {cards.map((item) => (
-            <View style={styles.creditCard}>
+            <View key={item.numero} style={styles.creditCard}>
               <View>
                 <Text style={styles.numberCard}>{item.numero}</Text>
                 <Text style={styles.nameCard}>{item.nome}</Text>
 
                 <View style={styles.footerCard}>
-                  <Text style={styles.expirationCard}>
-                    {item.dataExpiracao}
+                  <Text style={styles.expirationDateCard}>
+                    Valid. {item.dataExpiracao}
                   </Text>
-                  <Text style={styles.expirationCard}>{item.cvv}</Text>
+                  <Text style={styles.expirationDateCard}>Cvv: {item.cvv}</Text>
                 </View>
               </View>
             </View>
           ))}
         </ScrollView>
       </View>
+
+      <Text style={styles.myCards}>Minhas transações</Text>
+
+      <FlatList
+        data={transactions}
+        keyExtractor={(item) => item.data}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.dateItem}>{item.data}</Text>
+
+            {item.items.map((transactionItem) => (
+              <View key={transactionItem.id} style={styles.transactionItem}>
+                <View style={styles.leftContentTransactionItem}>
+               
+                  {transactionItem.tipo === TransactionType.ENTRADA ? (
+                    <Icon name="plus-circle" size={30} color="#26d826" />
+                  ) : (
+                    <Icon name="minus-circle" size={30} color="#d82926" />
+                  )}
+
+                  <Text style={styles.descriptionTransactionItem}>
+                    {transactionItem.descricao}
+                  </Text>
+                </View>
+                <Text style={styles.valueTransactionItem}>
+                  R$ {transactionItem.valor}
+                </Text>
+              </View>
+            ))}
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -65,37 +134,68 @@ const styles = StyleSheet.create({
   },
   creditCard: {
     width: Dimensions.get("window").width - 40,
-    height: 190,
-    borderRadius: 10,
+    height: 200,
+    borderRadius: 8,
     backgroundColor: "#150230",
-    gap: 20,
     justifyContent: "flex-end",
     marginLeft: 20,
   },
   numberCard: {
     fontSize: 22,
-    color: "#fff",
+    color: "#FFF",
     fontWeight: "bold",
-    margin: 5,
-  },
-  nameCard: {
-    fontSize: 13,
-    color: "#fff",
-    margin: 10,
+    margin: 20,
     marginVertical: 5,
   },
-  expirationCard: {
-    fontSize: 10,
-    color: "#fff",
+  myCards: {
+    fontSize: 22,
+    color: "#000",
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  nameCard: {
+    fontSize: 22,
+    color: "#FFF",
+    fontWeight: "bold",
+    margin: 20,
+    marginVertical: 5,
+  },
+  expirationDateCard: {
+    fontSize: 18,
+    color: "#FFF",
   },
   footerCard: {
     flexDirection: "row",
     justifyContent: "space-between",
+    margin: 20,
   },
-  myCards: {
+  transactionItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#150230",
+    height: 54,
+    borderRadius: 8,
+    padding: 5,
+    marginBottom: 20,
+  },
+  leftContentTransactionItem: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+  },
+  descriptionTransactionItem: {
+    fontSize: 18,
+    color: "#FFF",
+  },
+  valueTransactionItem: {
+    fontSize: 20,
+    color: "#FFF",
+  },
+  dateItem: {
     fontSize: 22,
-    fontWeight: "bold",
     color: "#000",
+    marginVertical: 20,
   },
 });
 
